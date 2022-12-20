b7ee10417da982cdb34a4d8c70d50f61503e4e91
juan <henriquejuan36@gmail.com>
2022-12-20 10:36:29 -0300

    javaScript1

---
+++ propject/max_min.js
@@ -0,0 +1,13 @@
+// Escreva uma função que usa 2 númeris e retorna o maior entre eles
+
+function max(n1, n2){
+    if (n1 > n2){
+        console.log(n1,'é maior que o', n2)
+    }
+    else{
+        console.log(n2,'é maior que o', n1)
+    }
+}
+
+max(4,5)
+max(10,1)
