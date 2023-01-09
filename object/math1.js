906244bf53aab7d15a0f8bf7f026d265ffc0b017
juan <henriquejuan36@gmail.com>
2023-01-09 18:00:03 -0300

    obf

---
+++ object/math1.js
@@ -0,0 +1,11 @@
+let max = Math.max(3,6,8,10)
+let min = Math.min(3,6,8,10)
+let random = Math.random(0,15)
+
+
+
+
+console.log(max)
+console.log(min)
+console.log(random)
+
---
+++ object/string.js
@@ -0,0 +1,10 @@
+// Tipo primitivo
+const mensagem = 'minha primeira mensagem'
+// tipo ojeto
+const outraMensagem = new String('Bom dia')
+
+typeof (mensagem)
+
+mensagem.includes('primeiro')
+mensagem.includes('vermelho')
+mensagem.startsWith('vermelho')
