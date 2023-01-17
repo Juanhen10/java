cf702abe659f1d35779821ced49de19f90228f0f
juan <henriquejuan36@gmail.com>
2023-01-17 16:50:38 -0300

    template linear

---
+++ object/tamplate_literal.js
@@ -0,0 +1,26 @@
+// JavaScript - Template literal
+const mensagem = 
+'oi isso é minha\n' +
+'\'primeira\' msg';
+
+// Object {}
+// Boolean true-false {}
+// string '',""
+// Template ``
+
+const outra = `oi isso é a minha
+'primeira' mensagem`;
+
+
+const nome = 'padri';
+const msg = 'Olá' + nome + '\n'
+
+const email = 
+`Olá ${nome}
+
+seu email foi efetuado
+Com
+Sucesso
+
+obrigado
+Juan.`;
