b1c94e2465e41c30e415b5c2c1b65dfb8f52650a
juan <henriquejuan36@gmail.com>
2023-02-01 18:26:15 -0300

    combination

---
+++ arrays/combination.js
@@ -0,0 +1,12 @@
+// Combinando Arrays
+const numero = [1,2,3,4,5]
+
+const combinado = numero.join('.');
+console.log(combinado);
+
+const frase = "olha bem vindo ao curso";
+const resultado = frase.split(' ');
+
+console.log(resultado);
+
+console.log(resultado.join('-'))
