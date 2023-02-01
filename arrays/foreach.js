a30465269c51e5063fa969d94cab5c168c7b76f8
juan <henriquejuan36@gmail.com>
2023-02-01 17:30:01 -0300

    foreach

---
+++ arrays/foreach.js
@@ -0,0 +1,9 @@
+// JAVASCRIPT ITERANDO UM ARRAY
+const numeros = [1,2,3,4,5];
+
+// for (numero of numeros)
+    // console.log(numero);
+
+// forEach
+
+numeros.forEach((numero,indice) => console.log(numero, indice))
